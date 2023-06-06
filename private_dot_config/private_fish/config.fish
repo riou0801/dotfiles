0ffb87e2745989fa5b070f199e102e42680785a8
riou0801 <guregorio8970@gmail.com>
2023-06-06 12:58:35 +0900

    second commit

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	#set proxy_host 172.16.1.2:15080
+	#set proxy_auth false
+	
+	fzf_configure_bindings --directory=\cf
+	
+	#abbr
+	abbr -a exa exa -T --all --icons --color=always
+	abbr -a fd fd -Hg
+	abbr -a pacs sudo pacman -Syu
+	abbr -a bat bat --color always
+	abbr -a du dust -r
+	abbr -a er erd --disk-usage logical --level 2 --human --icons --sort size --layout inverted --dir-order first
+	abbr -a tclock tclock --color '#a6adc8'
+	abbr -a ba baph -uanN
+end
+
+zoxide init fish | source
