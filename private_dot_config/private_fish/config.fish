ef9f7c3302ca53160d56b79d6958a911e6348f61
riou0801 <guregorio8970@gmail.com>
2023-07-14 15:57:19 +0900

    update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -11,7 +11,8 @@ if status is-interactive
 	abbr -a pacs sudo pacman -Syu
 	abbr -a bat bat --color always
 	abbr -a du dust -r
-	abbr -a er erd --disk-usage logical --level 2 --human --icons --sort size --layout inverted --dir-order first
+	#abbr -a er erd --disk-usage logical --level 2 --human --icons --sort size --layout inverted --dir-order first
+    abbr -a erd erd -H -I -i -P --hidden --layout inverted --dir-order first  --level 2
 	abbr -a tclock tclock --color '#a6adc8'
 	abbr -a ba baph -uanN
 end
